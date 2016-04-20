e67e3e3c4a85b53f5de79d99c03fb14078a75079
SamuelColacchia <samuel.colacchia@gordon.edu>
2016-04-20 12:39:24 -0400

    Basic input method implimented|towns work, roads do not

--- project5.cc
+++ project5.cc
@@ -2,62 +2,72 @@
 
 // FILE *_stream;
 
-char something;
+int numTowns;
+int numRoads;
+string townInput;
+string roadInput;
 
 int main(int argc, char const *argv[])
 {
   cout << "Project 5" << endl;
-  while (true)
+
+  cout << endl;
+
+  cout << "(#TOWNS #ROADS)" << endl;
+
+  cin >> numTowns;
+
+  cin >> numRoads;
+
+  cout << "towns:" << numTowns << endl;
+
+  cout << "roads:" << numRoads << endl;
+
+  for (int loopNumTowns = 0; loopNumTowns < numTowns; loopNumTowns++)
   {
-
-    cin >> something;
-
-
-
-    if (cin.eof())
-    {
-      break;
-    }
-
-
-
-    cout << "Echo input:" << something << endl;
+    cin >> townInput;
+    cout << "towninput:" << townInput << endl;
   }
 
+  for (int loopNumRoads = 0; loopNumRoads < numRoads; loopNumRoads++)
+  {
+    cin >> roadInput;
+    cout << "roadinput:" << roadInput << endl;
+  }
 
-  // while (true)
-  // {
-  //   if (cin.eof())
-  //   {
-  //     break;
-  //   }
-  //   else
-  //   {
-  //     cin.unget();
-  //   }
-  //   cout << cin << endl;
-  // }
+// while (true)
+// {
+//   if (cin.eof())
+//   {
+//     break;
+//   }
+//   else
+//   {
+//     cin.unget();
+//   }
+//   cout << cin << endl;
+// }
 
-  // ifstream file ("sampledata.data");
-  // while (getline(file,inputD)) {
-  //   cout << "current line:" << inputD << endl;
-  //   cout << "line pos 0:" << inputD[0] << endl;
-  //   cout << "line length:" << inputD.length() << endl;
-  // }
+// ifstream file ("sampledata.data");
+// while (getline(file,inputD)) {
+//   cout << "current line:" << inputD << endl;
+//   cout << "line pos 0:" << inputD[0] << endl;
+//   cout << "line length:" << inputD.length() << endl;
+// }
 
-  // while (!cin.eof())
-  // {
-  //   cout <<  "mats method" << inputD << endl;
-  //   getline(cin,inputD);
-  // }
+// while (!cin.eof())
+// {
+//   cout <<  "mats method" << inputD << endl;
+//   getline(cin,inputD);
+// }
 
-  // _stream = fopen(testD.c_str(), "r+b");
-  // if (_stream != NULL)
-  // {
-  //   cout << "there is content" << endl;
-  // }
-  // else
-  // {
-  //   cout << "empty" << endl;
-  // }
+// _stream = fopen(testD.c_str(), "r+b");
+// if (_stream != NULL)
+// {
+//   cout << "there is content" << endl;
+// }
+// else
+// {
+//   cout << "empty" << endl;
+// }
 }
