28035e05a906dd3be36dd1494529b089b5991074
SamuelColacchia <samuel.colacchia@gordon.edu>
2016-04-26 17:10:49 -0400

    Able to get input for both roads and towns

--- project5.cc
+++ project5.cc
@@ -16,10 +16,10 @@ int roadInputDistPos;
 
 int endNull;
 
-char roadT1;
+string roadT1;
 string roadT2;
 string roadBridge;
-string roadDist;
+int roadDist;
 
 int main()
 {
@@ -34,90 +34,97 @@ int main()
 
   cout << "(#TOWNS #ROADS)" << endl;
 
-  while (true)
+  if (cin.eof())
   {
-    if (cin.eof())
-    {
-      std::cout << "no more Data" << std::endl;
-    }
-    cin >> numTowns;
-
-    cin >> numRoads;
-
-    cout << "towns:" << numTowns << endl;
-
-    cout << "roads:" << numRoads << endl;
-
-    for (int loopNumTowns = 0; loopNumTowns < numTowns; loopNumTowns++)
-    {
-      if (loopNumTowns == 0)
-      {
-        cin >> regionCapital;
-
-        cout << "regionCapital:" << regionCapital << endl;
-
-        theGraph.addTown(regionCapital, true);
-
-      }
-      else
-      {
-        cin >> townInput;
-        cout << "towninput:" << townInput << endl;
-        theGraph.addTown(regionCapital, false);
-      }
-
-      //use the created graph to create towns
-    }
-    getline(cin,roadInput);
-    for (int loopNumRoads = 0; loopNumRoads < numRoads; loopNumRoads++)
-    {
-      // split( fields, getline(cin,roadInput), is_any_of( " " ));
-      //
-      // string heads = fields[0];
-      // string tails = feilds[1];
-      // double distance = feilds[2];
-      // bool bridge;
-      // if(fields[3] == B){
-      //   bridge = true;
-      // }
-      // else[
-      //   bridge = false;
-      // ]
-
-      // roadInput.find(' ')
-
-      roadInputSize = roadInput.size();
-
-      for (int roadPos = 0; roadInputSize > roadPos; roadPos++)
-      {
-        if (roadInput[roadPos] == ' ') {
-          roadInputT1Pos = roadPos;
-          endNull = roadInput.copy(roadT1, 0, roadInputT1Pos);
-
-          roadT1[endNull] = '\n';
-          roadPos++;
-        }
-        else if (roadInput[roadPos] == ' ') {
-          roadInputT2Pos = roadPos;
-          roadPos++;
-        }
-        else if (roadInput[roadPos] == ' ') {
-          roadInputBridgePos = roadPos;
-          roadPos++;
-        }
-        else if (roadInput[roadPos] == ' ') {
-          roadInputDistPos = roadPos;
-          roadPos++;
-        }
-      }
-
-
-      cout << "T1:" << roadT1 << endl;
-      cout << "roadinput:" << roadInput << endl;
-      //use the created graph to create roads
-    }
-    getline(cin,roadInput);
+    std::cout << "no more Data" << std::endl;
   }
+  cin >> numTowns;
+
+  cin >> numRoads;
+
+  cout << "towns:" << numTowns << endl;
+
+  cout << "roads:" << numRoads << endl;
+
+  for (int loopNumTowns = 0; loopNumTowns < numTowns; loopNumTowns++)
+  {
+    if (loopNumTowns == 0)
+    {
+      cin >> regionCapital;
+
+      cout << "regionCapital:" << regionCapital << endl;
+
+      theGraph.addTown(regionCapital, true);
+    }
+    else
+    {
+      cin >> townInput;
+      cout << "towninput:" << townInput << endl;
+      theGraph.addTown(regionCapital, false);
+    }
+  }
+  while (cin.get() != '\n')
+  {
+  }
+  for (int loopNumRoads = 0; loopNumRoads < numRoads; loopNumRoads++)
+  {
+    cin >> roadT1 >> roadT2 >> roadBridge >> roadDist;
+
+    cout << "roadT1:" << roadT1 << " roadT2:" << roadT2 << " roadBridge:" << roadBridge << " roadDist:" << roadDist << endl;
+    
+  }
+
+
+
+  //   for (int loopNumRoads = 0; loopNumRoads < numRoads; loopNumRoads++)
+  //   {
+  //     // split( fields, getline(cin,roadInput), is_any_of( " " ));
+  //     //
+  //     // string heads = fields[0];
+  //     // string tails = feilds[1];
+  //     // double distance = feilds[2];
+  //     // bool bridge;
+  //     // if(fields[3] == B){
+  //     //   bridge = true;
+  //     // }
+  //     // else[
+  //     //   bridge = false;
+  //     // ]
+  //
+  //     // roadInput.find(' ')
+  //
+  //     // roadInputSize = roadInput.size();
+  //     //
+  //     // for (int roadPos = 0; roadInputSize > roadPos; roadPos++)
+  //     // {
+  //     //   if (roadInput[roadPos] == ' ') {
+  //     //     roadInputT1Pos = roadPos;
+  //     //     endNull = roadInput.copy(roadT1, 0, roadInputT1Pos);
+  //     //
+  //     //     roadT1[endNull] = '\n';
+  //     //     roadPos++;
+  //     //   }
+  //     //   else if (roadInput[roadPos] == ' ') {
+  //     //     roadInputT2Pos = roadPos;
+  //     //     roadPos++;
+  //     //   }
+  //     //   else if (roadInput[roadPos] == ' ') {
+  //     //     roadInputBridgePos = roadPos;
+  //     //     roadPos++;
+  //     //   }
+  //     //   else if (roadInput[roadPos] == ' ') {
+  //     //     roadInputDistPos = roadPos;
+  //     //     roadPos++;
+  //     //   }
+  //     // }
+  //
+  //
+  //     cout << "T1:" << roadT1 << endl;
+  //     cout << "roadinput:" << roadInput << endl;
+  //     //use the created graph to create roads
+  //   }
+  //   getline(cin,roadInput);
+  // }
 
 // while (true)
 // {
